36a5f7d77a4970cd4d86073b7818baa7359448f7
Pourna <sspourna.shoden@gmail.com>
2021-03-03 22:36:17 +0530

    Reafactor exisitng code and updated gitignore

--- spec/controller/application_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe ApplicationController, 'exception_handling', type: :controller do
-  it { should rescue_from(ActionController::RoutingError).with(:render_404) }
-end
